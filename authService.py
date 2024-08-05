7e18c7549fd9a930161f7a704e316c45b94feff0
Holger Cremer <>
2024-08-05 20:57:12 +0200

    Using logging instead of 'print', this fixes also issues with newer python versions

--- authService.py
+++ authService.py
@@ -6,7 +6,9 @@
 import json
 
 from MoinMoin import user
+from MoinMoin import log
 
+logger = log.getLogger(__name__)
 
 def execute(pagename, request):
     if request.method != "POST":
@@ -20,22 +22,26 @@ def execute(pagename, request):
         request.write("No auth configured")
         return
 
-    auth = request.environ.get('HTTP_AUTH_TOKEN')
-    if auth is None or auth != token:
-        request.status_code = 403
-        request.write("Invalid auth")
-        return
-
-    do = request.values.get('do')
-    if do == "list":
-        _list_user(request)
-        return
-    elif do == "loginCheck":
-        _login_check(request)
-        return
-    elif do == "isInGroup":
-        _is_in_group(request)
-        return
+    try:
+        auth = request.environ.get('HTTP_AUTH_TOKEN')
+        if auth is None or auth != token:
+            logger.info("Invalid auth (missing HTTP_AUTH_TOKEN")
+            request.status_code = 403
+            request.write("Invalid auth")
+            return
+        
+        do = request.values.get('do')
+        if do == "list":
+            _list_user(request)
+            return
+        elif do == "loginCheck":
+            _login_check(request)
+            return
+        elif do == "isInGroup":
+            _is_in_group(request)
+            return
+    except Exception as e:
+        logger.error("the error: %s" % e, e)
 
 
 def _send_json(request, data_to_send):
@@ -76,7 +82,6 @@ def _login_check(request):
     :param request:
     """
     data = json.loads(request.stream.read())
-    print "Checking for user %s" % data["login"]
     u = user.User(request, None, data["login"], password=data["password"])
     if u.valid == 1:
         result = 'ok'
@@ -91,6 +96,8 @@ def _login_check(request):
     if u.valid and data.get("listGroups"):
         groups = list(request.groups.groups_with_member(u.name))
         result_json["groups"] = groups
+    
+    logger.info("Checking for user %s: %s" % (data["login"], result_json))
     _send_json(request, result_json)
 
 
@@ -100,7 +107,7 @@ def _is_in_group(request):
     :param request:
     """
     data = json.loads(request.stream.read())
-    print "Is user %s in group %s" % (data["login"], data["group"])
+    logger.debug("Is user %s in group %s" % (data["login"], data["group"]))
     group = request.groups.get(data["group"])
     in_group = False
     if group is not None:
